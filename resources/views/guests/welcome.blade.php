2cad40308e7145a82f6d5281b754e166fd0256df
MarioBroi <inoix16@gmail.com>
2024-05-09 14:18:03 +0100

    edit card in the welcome page with some style

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -2,13 +2,27 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row row-cols-3 row-cols-lg-5 py-3 g-4">
 
             @forelse ($movies as $movie)
                 <div class="col">
                     <div class="card">
                         <div class="card-body">
-                            {{ $movie->title }}
+                            <h4 class="card-title">
+                                Titolo: {{ $movie->title }}
+                            </h4>
+                            <div class="card-text">
+                                Titolo originale: {{ $movie->original_title }}
+                            </div>
+                            <div class="card-text">
+                                Nazionalitá: {{ $movie->nationality }}
+                            </div>
+                            <div class="card-text">
+                                Data di uscita: {{ $movie->date }}
+                            </div>
+                            <div class="card-text">
+                                Voto: {{ $movie->vote }}
+                            </div>
                         </div>
                         <!-- /.card-body -->
                     </div>
