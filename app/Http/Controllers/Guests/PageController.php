8f2acc9b227a960c7974938cad17bb66756aea3d
MarioBroi <inoix16@gmail.com>
2024-05-09 13:57:00 +0100

    add route to web.php for the home page, add method to PageController, add titles in partials and main page

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('guests.welcome');
+    }
 }
--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.app')
 
 @section('content')
+    <h1>page main</h1>
 @endsection
